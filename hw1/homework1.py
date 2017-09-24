c80eccbeaa602b7c50c32cae7980477bb0623b6b
daoleynik1 <31985736+daoleynik1@users.noreply.github.com>
2017-09-25 06:48:24 +0900

    Add files via upload

---
+++ hw1/homework1.py
@@ -0,0 +1,13 @@
+a = int(input("type the first number (A)"))
+b = int(input("type the second number (B)"))
+c = int(input("type the third number (C)"))
+print("\n")
+
+if a + b == c:
+    print ("yes, the third number is the sum of the first one and the second one")
+else:
+    print ("no, the third number is NOT the sum of the first one and the second one")
+if a / b == c:
+    print ("yes, the third number is the quotient of the first one and the second one")
+else:
+    print ("no, the third number is NOT the quotient of the first one and the second one")
